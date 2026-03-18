df6f9bcb4e2d242d06291d368250ec6db6592149
masud3239 <mdsikdermasud@gmail.com>
2026-03-18 20:04:17 +0600

    my app make

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div className="navbar bg-base-100 shadow-sm">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Link</a></li>
+      <li>
+        <details>
+          <summary>Parent</summary>
+          <ul className="bg-base-100 rounded-t-none p-2">
+            <li><a>Link 1</a></li>
+            <li><a>Link 2</a></li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+  </div>
+</div>
+          <h1 className="bg-amber-500 text-center">Get started</h1>
+
+          <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content text-center">
+    <div className="max-w-md">
+      <h1 className="text-5xl font-bold">Hello there</h1>
+      <p className="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+          
+           
+        
+    </>
+  )
+}
+
+export default App
